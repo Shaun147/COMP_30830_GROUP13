4dbb7c1ee4119ed2550465d78d7bc17d71ada894
Zhaohuan-Guo <zhaohuan.guo@ucdconnect.ie>
2023-03-03 15:44:31 +0000

    update

---
+++ test.py
@@ -0,0 +1,163 @@
+from ctypes.wintypes import PLARGE_INTEGER
+from datetime import datetime
+import json
+import pickle
+import pymysql
+from sqlalchemy import *
+from sqlalchemy import Table, Column, Integer, Float, String, DateTime
+from sqlalchemy import MetaData
+from sqlalchemy import create_engine
+from flask import Flask, jsonify, render_template, request
+from jinja2 import Template
+from sqlalchemy import create_engine, select, MetaData, Table, and_
+import pandas as pd
+from joblib import dump, load
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+app = Flask(__name__)
+NAME = "Dublin"
+STATIONS = "https://api.jcdecaux.com/vls/v1/stations"
+APIKEY = "8baeb25cfb9a5eb1f76dec99338e19bcd20e4386"
+USER = "group13"
+PASSWORD = "123456789"
+HOST = "dublinbikegroup13.c1msfserw61n.us-east-1.rds.amazonaws.com"
+PORT = 3306
+DATABASE = "dbbike13"
+
+
+@app.route("/")
+def hello():
+    # return 'Welcome to My Watchlist!'
+
+    return render_template("index.html")
+
+
+@app.route("/stations")
+def stations():
+    engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, HOST, PORT, DATABASE), echo=True)
+    connection = engine.connect()
+
+    sql = "SELECT s.number,s.bike_stands, s.name, s.address, s.position_lat, s.position_lng, a.available_bike_stands, a.available_bikes, " \
+          "a.status, MAX(a.last_update) AS `current_availability` " \
+          "FROM dbbike13.availability as a " \
+          "INNER JOIN dbbike13.station as s ON s.number = a.number " \
+          "GROUP BY s.number " \
+          "ORDER BY s.number;"
+
+    df = pd.read_sql(sql, engine)
+    print(df)
+
+    return df.to_json(orient="records")
+
+
+@app.route("/static_stations")
+def static_stations():
+    engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, HOST, PORT, DATABASE), echo=True)
+    connection = engine.connect()
+
+    sql = "SELECT * FROM dbbike13.station " \
+          "ORDER BY name;"
+
+    df = pd.read_sql(sql, engine)
+
+    return df.to_json(orient="records")
+
+
+@app.route('/occupancy/<int:station_id>')
+def get_occupancy(station_id):
+    engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, HOST, PORT, DATABASE), echo=True)
+    connection = engine.connect()
+
+    sql = f"""SELECT s.name, avg(a.available_bike_stands) as Avg_bike_stands,
+        avg(a.available_bikes) as Avg_bikes_free, DAYNAME(a.last_update) as DayName
+        FROM dbbike13.availability as a
+        JOIN dbbike13.station as s
+        ON s.number = a.number
+        WHERE s.number = {station_id}
+        GROUP BY s.name , DayName 
+        ORDER BY s.name , DayName;"""
+
+    df = pd.read_sql(sql, engine)
+
+    return df.to_json(orient="records")
+
+
+@app.route('/hourly/<int:station_id>')
+def get_hourly_data(station_id):
+    engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, HOST, PORT, DATABASE), echo=True)
+    connection = engine.connect()
+
+    sql = f"""SELECT s.name,count(a.number),avg(available_bike_stands) as Avg_bike_stands,
+        avg(available_bikes) as Avg_bikes_free,EXTRACT(HOUR FROM last_update) as Hourly
+        FROM dbbike13.availability as a
+        JOIN dbbike13.station as s
+        ON s.number = a.number
+        WHERE a.number = {station_id}
+        GROUP BY EXTRACT(HOUR FROM last_update) 
+        ORDER BY EXTRACT(HOUR FROM last_update) asc"""
+
+    df = pd.read_sql(sql, engine)
+
+    return df.to_json(orient="records")
+
+
+@app.route("/weather_forecast")
+def weather_forecast():
+    engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, HOST, PORT, DATABASE), echo=True)
+    connection = engine.connect()
+    print("************************")
+
+    sql = f"""SELECT weather_description, weather_main, humidity, wind_speed,visibility,sunrise,sunset,pressure 
+    FROM dbbike13.weather_Dublin
+    ORDER BY dt DESC
+    LIMIT 1;"""
+
+    df = pd.read_sql(sql, engine)
+
+    return df.to_json(orient="records")
+
+
+@app.route('/predict/<int:station_id>')
+def predict_station(station_id):
+    print("************************")
+
+    with open('./static/pklFile/model_{}.pkl'.format(station_id), 'rb') as handle:
+        model = pickle.load(handle)
+
+        now_hour = datetime.now().hour
+
+        now_hour += 1
+        prediction = model.predict(
+            [[now_hour, 56.0, 283.67, 79.0, 1009.0, 75.0, 10000.0, 9.26, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0]])
+        print(
+            "pklfine pklfine pklfine pklfine pklfine pklfine pklfine pklfine pklfine pklfine pklfine pklfine pklfine pklfine pklfine pklfine")
+
+        predict_list = prediction.tolist()
+        result = str(predict_list[0])
+
+        print(result)
+        return result
+
+    # input_model = station_dfs[ID][modelfeatures]
+    # output = station_dfs[ID]['available_bikes']
+    # modelfeature = ['hour_x','minute_x', 'temp', 'humidity', 'pressure', 'Clouds', 'visibility',
+    # 'wind_speed', 'weekday_Friday', 'weekday_Monday', 'weekday_Saturday','weekday_Sunday', 'weekday_Thursday', 'weekday_Tuesday',
+    # 'weekday_Wednesday', 'weather_main_Clear', 'weather_main_Clouds', 'weather_main_Drizzle', 'weather_main_Rain']
+
+    # engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, HOST, PORT, DATABASE), echo=True)
+    # connection = engine.connect()
+
+    # sql = "SELECT temp, humidity, pressure, Clouds, visibility, wind_speed, dt, weather_main FROM test.future_weather " \
+    #    "Where dt == datetime.now().hour;"
+    #### +1, +2
+
+    # weather = pd.read_sql(sql, engine)
+    ### the code used in ML learning
+    ### if else sentence to change 'dt' into 'weekday_Monday' and so on ...
+    ### if else sentence to change 'weather_main' into 'weather_main_Clear' and so on ...
+
+
+if __name__ == "__main__":
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=False)
---
+++ weather_test.py
@@ -0,0 +1,102 @@
+import pymysql
+import traceback
+import requests
+import time
+from datetime import datetime
+import json
+
+weather_apiKey = "e7138528cfa0e09e1ad22a15e2e2532a"
+city_name = 'Dublin,ie'
+parameters = {"q": city_name, "appid": weather_apiKey}
+weather_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+print('work')
+
+
+def create_table():
+    sql = """
+    CREATE TABLE IF NOT EXISTS weather_Dublin(
+        Clouds INTEGER,
+        feels_like DOUBLE,
+        humidity INTEGER,
+        pressure INTEGER,
+        temp DOUBLE,
+        temp_max DOUBLE,
+        temp_min DOUBLE,
+        sunrise VARCHAR(255),
+        sunset VARCHAR(255),
+        visibility INTEGER,
+        weather_description VARCHAR(255),
+        weather_main VARCHAR(255),
+        wind_deg INTEGER,
+        wind_speed DOUBLE,
+        dt VARCHAR(255) PRIMARY KEY
+    );
+    """
+
+    try:
+        cursor.execute(sql)
+        print('create ok')
+    except Exception as e:
+        print(e)
+
+
+def write_to_db(text):
+    weather_data = json.loads(text)
+    weather_vals = (
+        str(weather_data['clouds']['all']),
+        str(weather_data['main']['feels_like']),
+        str(weather_data['main']['humidity']),
+        str(weather_data['main']['pressure']),
+        str(weather_data['main']['temp']),
+        str(weather_data['main']['temp_max']),
+        str(weather_data['main']['temp_min']),
+        str(datetime.fromtimestamp(weather_data['sys']['sunrise'])),
+        str(datetime.fromtimestamp(weather_data['sys']['sunset'])),
+        str(weather_data['visibility']),
+        str(weather_data['weather'][0]['description']),
+        str(weather_data['weather'][0]['main']),
+        str(weather_data['wind']['deg']),
+        str(weather_data['wind']['speed']),
+        str(datetime.fromtimestamp(weather_data['dt']))
+    )
+
+    sql = """
+    INSERT INTO `test`.`weather_Dublin` (`Clouds`, `feels_like`, `humidity`, 
+    `pressure`, `temp`, `temp_max`, `temp_min`, `sunrise`, `sunset`, `visibility`, 
+    `weather_description`, `weather_main`, `wind_deg`, `wind_speed`, `dt`) VALUES 
+    ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')
+    """ % weather_vals
+
+    try:
+        cursor.execute(sql)
+        db.commit()
+        print("insert ok")
+    except:
+        db.rollback()
+        print("insert wrong")
+
+    db.close()
+
+
+while True:
+    try:
+        db = pymysql.connect(
+            host="dublinbikegroup13.c1msfserw61n.us-east-1.rds.amazonaws.com",
+            user="group13",
+            password="123456789",
+            port=3306,
+            database="dbbike13")
+        cursor = db.cursor()
+        create_table()
+
+        now = datetime.now()
+        r = requests.get(weather_URL, params=parameters)
+        print(r, now)
+
+        write_to_db(r.text)
+
+        time.sleep(5 * 60)
+
+    except:
+        print(traceback.format_exc())
