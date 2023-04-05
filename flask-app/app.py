9a4af52a9ad2a5599564307ab6d3a9f92c6f0b7e
shizukuyu <94681454+shizukuyu@users.noreply.github.com>
2023-04-05 17:00:01 +0100

    testse file contains updated website layout

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,109 +1,66 @@
-from datetime import datetime
-from flask import Flask, jsonify, render_template, request, url_for,g
-from sqlalchemy import create_engine
-from flask_mysqldb import MySQL
+from sqlalchemy import *
+import flask
 import pandas as pd
-import traceback
-import functools
-import requests
-import os
 
-app = Flask(__name__)
 
-app.config.from_object("config.Config")
+app = flask.Flask(__name__)
+NAME = "Dublin"
+STATIONS = "https://api.jcdecaux.com/vls/v1/stations"
+APIKEY = "8baeb25cfb9a5eb1f76dec99338e19bcd20e4386"
+USER = "group13"
+PASSWORD = "123456789"
+HOST = "dublinbikegroup13.c1msfserw61n.us-east-1.rds.amazonaws.com"
+PORT = 3306
+DATABASE = "dbbike13"
 
-def connect_to_database():
-    engine = create_engine(
-        f"mysql://{app.config['USER']}:{app.config['PASSWORD']}@{app.config['HOST']}:{app.config['PORT']}/{app.config['DATABASE']}",
-        echo=True
-    )
-    return engine
 
-# Use Flask's g object to store a reference to the database connection object
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = connect_to_database()
-    return db
-
-# Close the database connection when the application context is destroyed
-@app.teardown_appcontext
-def close_db(error):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.dispose()
-
+engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, HOST, PORT, DATABASE), echo=True)
 
 @app.route("/")
 def hello():
-    return render_template("index1.html")
+    # return "hello"
+    return flask.render_template("index1.html")
 
 
 @app.route("/stations")
-@functools.lru_cache(maxsize=128)
-def get_stations():
-    engine = get_db()
-        
-    sql = "SELECT s.number, s.bike_stands, s.name, s.address, s.position_lat, s.position_lng, a.available_bike_stands, a.available_bikes, " \
-        "a.status, MAX(a.last_update) AS `current_availability` " \
-        "FROM availability AS a " \
-        "INNER JOIN station AS s ON s.number = a.number " \
-        "GROUP BY s.number " \
-        "ORDER BY s.number;"
-          
-    try:
-        with engine.connect() as conn:
-            rows = conn.execute(sql).fetchall()
-            print(f"#found {len(rows)} stations", rows)
-            return jsonify([dict(row) for row in rows])
-    except Exception as e:
-        print(traceback.format_exc())
-        return f"error in get_stations: {e}", 404
-
+def stations():
+    sql = """
+    SELECT s.number,s.bike_stands, s.name, s.address, s.position_lat, 
+    s.position_lng, a.available_bike_stands, a.available_bikes,
+    a.status, MAX(a.last_update) AS `current_availability`
+    FROM dbbike13.availability as a
+    INNER JOIN dbbike13.station as s 
+    WHERE s.number = a.number
+    GROUP BY s.number
+    ORDER BY s.number
+    """
+    rs = pd.read_sql(sql, engine)
+    print(rs)
+    return rs.to_json(orient="records")
 
 @app.route("/static_stations")
 def static_stations():
-    engine = get_db()
+    sql = """
+    SELECT * FROM dbbike13.station
+    ORDER BY name;
+    """
+    rs = pd.read_sql(sql, engine)
+    return rs.to_json(orient="records")
 
-    sql = "SELECT * FROM dbbikes1.station " \
-          "ORDER BY name;"
+@app.route("/weather")
+def weather():
+    sql = """SELECT weather_description, weather_main, humidity, 
+    wind_speed,visibility,sunrise,sunset,pressure,icon,temp
+    FROM dbbike13.weather
+    ORDER BY dt DESC
+    LIMIT 1;"""
+    rs = pd.read_sql(sql, engine)
+    print(rs.to_json(orient="records"))
+    return rs.to_json(orient="records")
 
-    try:
-        with engine.connect() as conn:
-            rows = conn.execute(sql).fetchall()
-            print(f"#found {len(rows)} stations", rows)
-            return jsonify([dict(row) for row in rows])
-    except Exception as e:
-        print(traceback.format_exc())
-        return f"error in get_stations: {e}", 404
 
-
-@app.route('/occupancy/<int:station_id>')
-def get_occupancy(station_id):
-    engine = get_db()
-
-    sql = f"""SELECT s.name, avg(a.available_bike_stands) as Avg_bike_stands,
-        avg(a.available_bikes) as Avg_bikes_free, DAYNAME(a.last_update) as DayName
-        FROM dbbikes1.availability as a
-        JOIN dbbikes1.station as s
-        ON s.number = a.number
-        WHERE s.number = {station_id}
-        GROUP BY s.name , DayName 
-        ORDER BY s.name , DayName;"""
-
-    try:
-        with engine.connect() as conn:
-            rows = conn.execute(sql).fetchall()
-            print(f"#found {len(rows)} stations", rows)
-            return jsonify([dict(row) for row in rows])
-    except Exception as e:
-        print(traceback.format_exc())
-        return f"error in get_stations: {e}", 404
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
 
 
 
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
---
+++ flask-app/test.py
@@ -0,0 +1,109 @@
+from datetime import datetime
+from flask import Flask, jsonify, render_template, request, url_for,g
+from sqlalchemy import create_engine
+from flask_mysqldb import MySQL
+import pandas as pd
+import traceback
+import functools
+import requests
+import os
+
+app = Flask(__name__)
+
+app.config.from_object("config.Config")
+
+def connect_to_database():
+    engine = create_engine(
+        f"mysql://{app.config['USER']}:{app.config['PASSWORD']}@{app.config['HOST']}:{app.config['PORT']}/{app.config['DATABASE']}",
+        echo=True
+    )
+    return engine
+
+# Use Flask's g object to store a reference to the database connection object
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+    return db
+
+# Close the database connection when the application context is destroyed
+@app.teardown_appcontext
+def close_db(error):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.dispose()
+
+
+@app.route("/")
+def hello():
+    return render_template("index1.html")
+
+
+@app.route("/stations")
+@functools.lru_cache(maxsize=128)
+def get_stations():
+    engine = get_db()
+        
+    sql = "SELECT s.number, s.bike_stands, s.name, s.address, s.position_lat, s.position_lng, a.available_bike_stands, a.available_bikes, " \
+        "a.status, MAX(a.last_update) AS `current_availability` " \
+        "FROM availability AS a " \
+        "INNER JOIN station AS s ON s.number = a.number " \
+        "GROUP BY s.number " \
+        "ORDER BY s.number;"
+          
+    try:
+        with engine.connect() as conn:
+            rows = conn.execute(sql).fetchall()
+            print(f"#found {len(rows)} stations", rows)
+            return jsonify([dict(row) for row in rows])
+    except Exception as e:
+        print(traceback.format_exc())
+        return f"error in get_stations: {e}", 404
+
+
+@app.route("/static_stations")
+def static_stations():
+    engine = get_db()
+
+    sql = "SELECT * FROM dbbikes1.station " \
+          "ORDER BY name;"
+
+    try:
+        with engine.connect() as conn:
+            rows = conn.execute(sql).fetchall()
+            print(f"#found {len(rows)} stations", rows)
+            return jsonify([dict(row) for row in rows])
+    except Exception as e:
+        print(traceback.format_exc())
+        return f"error in get_stations: {e}", 404
+
+
+@app.route('/occupancy/<int:station_id>')
+def get_occupancy(station_id):
+    engine = get_db()
+
+    sql = f"""SELECT s.name, avg(a.available_bike_stands) as Avg_bike_stands,
+        avg(a.available_bikes) as Avg_bikes_free, DAYNAME(a.last_update) as DayName
+        FROM dbbikes1.availability as a
+        JOIN dbbikes1.station as s
+        ON s.number = a.number
+        WHERE s.number = {station_id}
+        GROUP BY s.name , DayName 
+        ORDER BY s.name , DayName;"""
+
+    try:
+        with engine.connect() as conn:
+            rows = conn.execute(sql).fetchall()
+            print(f"#found {len(rows)} stations", rows)
+            return jsonify([dict(row) for row in rows])
+    except Exception as e:
+        print(traceback.format_exc())
+        return f"error in get_stations: {e}", 404
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
