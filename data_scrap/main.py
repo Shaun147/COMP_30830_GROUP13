80ded7323c200bd0e2fc87eff4085986456566d8
Zhaohuan-Guo <zhaohuan.guo@ucdconnect.ie>
2023-04-10 20:51:39 +0100

    Merge remote-tracking branch 'origin/Guo' into Guo

--- data_scrap/main.py
+++ data_scrap/main.py
@@ -24,7 +24,7 @@ RESOURCE_FUTURE_WEATHER = requests.get(future_weather_URL, params=parameters)
 def run_5m():
     weather_scrap.write_to_db_weather(RESOURCE_WEATHER.text)
     bike_scrap.write_to_db_availability(RESOURCE.text)
-    scheduler.enter(300, 1, run_5m)
+    scheduler.enter(10, 1, run_5m)
 
 
 def run_1h():
--- for_test/test1.py
+++ for_test/test1.py
@@ -4,12 +4,12 @@ import time
 
 def run_5m():
     print(1)
-    scheduler.enter(10, 1, run_5m)
+    scheduler.enter(5, 1, run_5m)
 
 
 def run_1h():
     print(2)
-    scheduler.enter(20, 1, run_1h)
+    scheduler.enter(10, 1, run_1h)
 
 
 scheduler = sched.scheduler(time.time, time.sleep)
