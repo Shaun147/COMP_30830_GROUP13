003a9642f6b0c786d7bd1187061db9a64cae7090
Zhaohuan-Guo <zhaohuan.guo@ucdconnect.ie>
2023-04-10 16:54:49 +0100

    update

--- data_scrap/update_pkl.py
+++ data_scrap/update_pkl.py
@@ -109,6 +109,5 @@ def update():
         with open('../machine_learning/station_' + str(i) + '.pkl', 'wb') as handle:
             pickle.dump(rf_list[i], handle, pickle.HIGHEST_PROTOCOL)
 
-update()
 
 
